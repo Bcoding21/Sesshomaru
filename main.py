1792ed41ef344a34fecc96fe076582eb8223a01f
brandoncole <brandoncole673@gmail.com>
2023-11-07 19:48:36 -0500

    refactor

--- main.py
+++ main.py
@@ -1,90 +1,74 @@
+from typing import Tuple
+
 import discord
 from discord.ext import commands, tasks
 from discord.utils import get
 import pytz
 import json
-import datetime
+from datetime import datetime, timedelta
+import logging
 
-jsonFile = "botConfig.json"  
-data = {}
-with open(jsonFile, "r") as f:
-    data = json.load(f)
-f.close()
+AMERICA_NEW_YORK_TIMEZONE = pytz.timezone("America/New_York")
+CONFIG_FILE_PATH = "botConfig.json"
 
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
+with open(CONFIG_FILE_PATH, "r") as file:
+    config = json.load(file)
+    logger.debug(f"Loaded configuration: {config}")
 
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-bot = commands.Bot(intents=intents, command_prefix = '.')
-
-# Define the event name and description.
-token = data["token"]
-guildId = data["guildId"]
-event_name = data["eventList"][0]["name"]
-event_description = data["eventList"][0]["description"]
-eventStartTimeHours = data["eventList"][0]["startTime"]["hour"]
-eventStartTimeMinutes = data["eventList"][0]["startTime"]["minute"]
-
-# Get the current time in UTC.
-utc_now = pytz.utc.localize(datetime.datetime.now())
-
-# Calculate the start and end times of the next event.
-dayOfWeek = datetime.datetime.now(pytz.timezone("America/New_York")).weekday()
-daysUntilMonday = (7 - dayOfWeek) % 7
-nextMonday = datetime.timedelta(days=daysUntilMonday) + datetime.datetime.now(pytz.timezone("America/New_York"))
-nextMonday = nextMonday.replace(hour=eventStartTimeHours, minute=eventStartTimeMinutes, second=0)
-
-next_event_start = nextMonday
-print(f"Start Time: {next_event_start}")
-next_event_end = next_event_start + datetime.timedelta(hours=1)
-print(f"End Time: {next_event_end}")
-
-guild = None
-event = {}
-
-@bot.event
-async def createEvent():
-    global guild
-    global event
-    
-    event = {
-        "name": event_name,
-        "description": event_description,
-        "start_time": next_event_start,
-        "end_time": next_event_end,
-        "entity_type": discord.EntityType.external,
-        "location": "",
-        "privacy_level": discord.PrivacyLevel.guild_only,
-    }
-
-@bot.event
-async def checkEventExists():
-    global guild
-    global event_name
-    events = await guild.fetch_scheduled_events()
-    for event in events:
-        if event.name == event_name:
-            return True
-    else:
-        return False
+bot = commands.Bot(intents=intents, command_prefix='.')
+guild = bot.get_guild(config["guildId"])
 
 
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+    logger.info(f"Logged in. Username: {bot.user}")
+    event_information = config["eventList"][0]
+    event_name = event_information["name"]
 
-    global guild
-    guild = bot.get_guild(guildId)
-
-    eventExists = await checkEventExists()
-    if eventExists:
-        global event_name
-        print(f"Event Name: {event_name} already exists")
+    if await does_event_exist(event_name):
+        logger.info(f"Event {event_name} already exist")
         return
-    
-    await createEvent()
-    print("Before scheduled event")
+
+    event = create_event(event_information)
+    logger.info(f"Creating event: {event}")
     await guild.create_scheduled_event(**event)
-    print("After Scheduled event")
+    logger.info(f"Successfully created event: {event_name}")
+
+
+async def does_event_exist(event_name: str) -> bool:
+    events = await guild.fetch_scheduled_events()
+    return any(event.name == event_name for event in events)
+
+
+def create_event(event_information: dict) -> dict:
+    event_start_hour = event_information["startTime"]["hour"]
+    event_start_minute = event_information["startTime"]["minute"]
+    start_date, end_date = _determine_start_and_end_date(event_start_hour, event_start_minute)
+
+    return {
+        "name": event_information["name"],
+        "description": event_information["description"],
+        "start_time": start_date,
+        "end_time": end_date,
+        "entity_type": discord.EntityType.external,
+        "privacy_level": discord.PrivacyLevel.guild_only,
+        "location": "",
+    }
+
+
+def _determine_start_and_end_date(start_hour: int, start_minute: int) -> Tuple[datetime, datetime]:
+    current_date = datetime.now(AMERICA_NEW_YORK_TIMEZONE)
+    days_until_monday = (7 - current_date.weekday()) % 7
+    next_mondays_date = timedelta(days=days_until_monday) + current_date
+    start_date = next_mondays_date.replace(hour=start_hour, minute=start_minute)
+    end_date = start_date + timedelta(hours=1)
+    return start_date, end_date
+
 
 # Start the bot.
-bot.run(token)
+bot.run(config["token"])
